1782849118297c36c752e308cdc521c5e1293542
gaohailang <ghlndsl@126.com>
2019-10-17 15:54:48 +0800

    chore(frontend): add the missing files and modify conf part for pai

--- frontend/load_heatmap.js
+++ frontend/load_heatmap.js
@@ -1,12 +1,166 @@
 /*
-Example files
+  change your API host here
+  we use convert python third-party module to convert our array-like data to 
+  matrix which is needed for heatmap visualizer
 */
 
+const tickDataAPIPrefix = 'http://106.75.91.214/keyvis?start=-60m&tag='
 var rawInfo,
   allRanges = []
 var heatmapType = 'written_bytes'
+
+let ctime = 0,
+  switches = {}
+
+/* 
+  leave it alone, or set it up by yourself
+  install the requirements.txt by pip, and run `python server`
+*/
+const convertAPI = 'http://106.75.91.214/convert'
+
+function getData(type) {
+  return fetch(tickDataAPIPrefix + type)
+    .then(res => res.json())
+    .then(json => {
+      rawInfo = json
+      try {
+        const tl =
+          '\t' +
+          '\t\t\t' +
+          json.heatmaps[0].values[0]
+            .map((i, idx) => {
+              return idx + 'm'
+              // return 'time-' + idx
+            })
+            .join('\t')
+
+        let dlines = [],
+          b_num = 0
+        json.heatmaps.forEach(h => {
+          h.ranges.forEach((i, idx) => {
+            const ds = h.labels[2] ? 'index ' + h.labels[2] : 'Data'
+            dlines.push(
+              [
+                `Bucket: bucket-${b_num}`,
+                'DB: ' + h.labels[0],
+                'Table: ' + h.labels[1],
+                'Data Type: ' + ds, //? h.labels[2] : 'DATA',
+                ...h.values[idx].map(i => i + 1)
+              ].join('\t')
+            )
+            b_num += 1
+          })
+        })
+
+        return tl + '\n' + dlines.join('\n')
+      } catch (e) {}
+    })
+}
+
+var about_string = ''
+
+function make_clust(type) {
+  $.busyLoadFull('show')
+
+  getData(type).then(data => {
+    fetch(convertAPI, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        data //: write(60, 256)
+      })
+    })
+      .then(res => res.json())
+      .then(json => {
+        console.log(json)
+
+        let network_data = json
+
+        var args = {
+          opacity_scale: 'log',
+          root: '#container-id-1',
+          network_data: network_data,
+          about: about_string,
+          sidebar_width: 0,
+          use_sidebar: false,
+          // 'ini_view':{'N_row_var':20}
+          ini_expand: true,
+          make_row_tooltip_handler: d => {
+            const x = rawInfo[0]['buckets'][d.rank]
+            return `from ${x['start']} to ${x['end']}`
+          },
+          make_col_tooltip_handler: d => {
+            return ''
+            return rawInfo[d.rank].time
+          },
+          matrix_tip_str_handler: (d, inst_value) => {
+            // const len = rawInfo.heatmaps[0].values.length
+            // const hidx = Math.floor(d.pos_y / len)
+            // const valIdx = d.pos_y % len
+            // const x = rawInfo.heatmaps[hidx].ranges[valIdx]
+            const x = allRanges[d.pos_y]
+
+            // const x = rawInfo[0]['buckets'][d.pos_y]
+            const row_name = `key range from ${JSON.stringify(
+              x['start']
+            )} to ${JSON.stringify(x['end'])}`
+            const col_name = '' // rawInfo[d.pos_x].time
+            tooltip_string =
+              '<p>' +
+              row_name +
+              ' and at time ' +
+              col_name +
+              '</p>' +
+              '<div> value: ' +
+              inst_value +
+              '</div>'
+            return tooltip_string
+          }
+        }
+        allRanges = []
+        rawInfo.heatmaps.forEach(h => {
+          allRanges = [...allRanges, ...h.ranges]
+        })
+
+        resize_container(args)
+
+        d3.select(window).on('resize', function() {
+          resize_container(args)
+          cgm.resize_viz()
+        })
+
+        cgm = Heatmap(args)
+
+        // check_setup_enrichr(cgm)
+
+        d3.select(cgm.params.root + ' .wait_message').remove()
+
+        $.busyLoadFull('hide')
+
+        $('.wait_message').html('Heatmap for ' + heatmapType)
+      })
+  })
+
+  // d3.json('json/' + inst_network, function(network_data) {
+  //   // define arguments object
+
+  // })
+}
+
+function resize_container(args) {
+  var screen_width = window.innerWidth
+  var screen_height = window.innerHeight - 20
+
+  d3.select(args.root)
+    .style('width', screen_width + 'px')
+    .style('height', screen_height + 'px')
+}
+
+make_clust(heatmapType)
+
 $('#x-switch-menu').click(e => {
-  console.log(e)
   // $('#svg_container-id-1').remove()
   // $('#svg_container-id-1').busyLoad('show')
   //   const canvas = $('.')
@@ -18,8 +172,7 @@ $('#x-switch-menu').click(e => {
   make_clust(t)
 })
 
-let ctime = 0,
-  switches = {}
+// write func to generate three types data matrixs for visualization
 function write(times, col) {
   const timesArr = Array(times)
     .fill(1)
@@ -91,207 +244,3 @@ function write(times, col) {
   ctime += 1
   return ret + valRet
 }
-
-function getData(type) {
-  return fetch('http://106.75.91.214/keyvis?start=-60m&tag=' + type)
-    .then(res => res.json())
-    .then(json => {
-      rawInfo = json
-      try {
-        const tl =
-          '\t' +
-          '\t\t\t' +
-          json.heatmaps[0].values[0]
-            .map((i, idx) => {
-               return idx+'m'
-		// return 'time-' + idx
-            })
-            .join('\t')
-
-        let dlines = [],
-          b_num = 0
-        json.heatmaps.forEach(h => {
-          h.ranges.forEach((i, idx) => {
-            const ds = h.labels[2] ? 'index ' + h.labels[2] : 'Data'
-            dlines.push(
-              [
-                `Bucket: bucket-${b_num}`,
-                'DB: ' + h.labels[0],
-                'Table: ' + h.labels[1],
-                'Data Type: ' + ds, //? h.labels[2] : 'DATA',
-                ...h.values[idx].map(i => i + 1)
-              ].join('\t')
-            )
-            b_num += 1
-          })
-        })
-
-        return tl + '\n' + dlines.join('\n')
-      } catch (e) {}
-      return
-      rawInfo = json
-      const tl =
-        '\t' +
-        json
-          .map((i, idx) => {
-            return 'time-' + idx
-          })
-          .join('\t')
-      const dlines = json[0]['buckets']
-        .map((t, idx) => {
-          return (
-            'bucket-' +
-            idx +
-            '\t' +
-            json
-              .map(j => {
-                const ret = j['buckets'][idx].value
-                return ret ? ret : 1
-              })
-              .join('\t')
-          )
-        })
-        .join('\n')
-
-      return tl + '\n' + dlines
-    })
-}
-
-var about_string = ''
-
-function make_clust(type) {
-  $.busyLoadFull('show')
-
-  getData(type).then(data => {
-    fetch('/convert', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-        // 'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: JSON.stringify({
-        data //: write(60, 256)
-      })
-    })
-      .then(res => res.json())
-      .then(json => {
-        console.log(json)
-
-        let network_data = json
-
-        var args = {
-	  opacity_scale: 'log',
-          root: '#container-id-1',
-          network_data: network_data,
-          about: about_string,
-          // row_tip_callback: hzome.gene_info,
-          col_tip_callback: test_col_callback,
-          tile_tip_callback: test_tile_callback,
-          dendro_callback: dendro_callback,
-          matrix_update_callback: matrix_update_callback,
-          cat_update_callback: cat_update_callback,
-          sidebar_width: 0,
-          use_sidebar: false,
-          // 'ini_view':{'N_row_var':20}
-          ini_expand: true,
-          make_row_tooltip_handler: d => {
-            const x = rawInfo[0]['buckets'][d.rank]
-            return `from ${x['start']} to ${x['end']}`
-          },
-          make_col_tooltip_handler: d => {
-            return ''
-            return rawInfo[d.rank].time
-          },
-          matrix_tip_str_handler: (d, inst_value) => {
-            // const len = rawInfo.heatmaps[0].values.length
-            // const hidx = Math.floor(d.pos_y / len)
-            // const valIdx = d.pos_y % len
-            // const x = rawInfo.heatmaps[hidx].ranges[valIdx]
-            const x = allRanges[d.pos_y]
-
-            // const x = rawInfo[0]['buckets'][d.pos_y]
-            const row_name = `key range from ${JSON.stringify(x['start'])} to ${JSON.stringify(x['end'])}`
-            const col_name = '' // rawInfo[d.pos_x].time
-            tooltip_string =
-              '<p>' +
-              row_name +
-              ' and at time ' +
-              col_name +
-              '</p>' +
-              '<div> value: ' +
-              inst_value +
-              '</div>'
-            return tooltip_string
-          }
-        }
-        allRanges = []
-        rawInfo.heatmaps.forEach(h => {
-          allRanges = [...allRanges, ...h.ranges]
-        })
-
-        resize_container(args)
-
-        d3.select(window).on('resize', function() {
-          resize_container(args)
-          cgm.resize_viz()
-        })
-
-        cgm = Heatmap(args)
-
-        // check_setup_enrichr(cgm)
-
-        d3.select(cgm.params.root + ' .wait_message').remove()
-
-        $.busyLoadFull('hide')
-
-        $('.wait_message').html('Heatmap for ' + heatmapType)
-      })
-  })
-
-  // d3.json('json/' + inst_network, function(network_data) {
-  //   // define arguments object
-
-  // })
-}
-
-function matrix_update_callback() {
-  if (genes_were_found[this.root]) {
-    enr_obj[this.root].clear_enrichr_results(false)
-  }
-}
-
-function cat_update_callback() {
-  console.log('callback to run after cats are updated')
-}
-
-function test_tile_callback(tile_data) {
-  var row_name = tile_data.row_name
-  var col_name = tile_data.col_name
-}
-
-function test_col_callback(col_data) {
-  var col_name = col_data.name
-}
-
-function dendro_callback(inst_selection) {
-  var inst_rc
-  var inst_data = inst_selection.__data__
-
-  // toggle enrichr export section
-  if (inst_data.inst_rc === 'row') {
-    d3.select('.enrichr_export_section').style('display', 'block')
-  } else {
-    d3.select('.enrichr_export_section').style('display', 'none')
-  }
-}
-
-function resize_container(args) {
-  var screen_width = window.innerWidth
-  var screen_height = window.innerHeight - 20
-
-  d3.select(args.root)
-    .style('width', screen_width + 'px')
-    .style('height', screen_height + 'px')
-}
-
-make_clust(heatmapType)
